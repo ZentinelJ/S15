b77bc3f0efb052634a5db0ee082b7b3394d9d268
villa <@ZentinelJ>
2025-06-05 09:44:40 -0500

    Se crea clase persona, se comienza encapsulando atributos de clase.

---
+++ src/Persona.java
@@ -0,0 +1,9 @@
+public class Persona {
+    //nombre, apellido, edad, genero, sueldo, hora, cargo
+    private String nombre;
+    private String apellido;
+    private String genero;
+    private String cargo;
+    private byte edad;
+    private byte hora;
+    private double sueldo;

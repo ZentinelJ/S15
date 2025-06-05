c61f50746b1e64cfb74cc5fbf7b43cb805831f12
villa <@ZentinelJ>
2025-06-05 09:46:10 -0500

    Generamos los set and get para trabajar con los atributos de nuestros objetos.

--- src/Persona.java
+++ src/Persona.java
@@ -17,3 +17,52 @@ public class Persona {
         this.hora = hora;
         this.sueldo = sueldo;
     }
+
+    public String getNombre(){
+        return nombre;
+    }
+    public String getApellido(){
+        return apellido;
+    }
+    public String getGenero(){
+        return genero;
+    }
+    public String getCargo(){
+        return cargo;
+    }
+    public byte getEdad(){
+        return edad;
+    }
+    public byte getHora(){
+        return hora;
+    }
+    public double getSueldo(){
+        return sueldo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+
+    public void setGenero(String genero){
+        this.genero = genero;
+    }
+
+    public void setCargo(String cargo){
+        this.cargo = cargo;
+    }
+
+    public void setEdad(byte edad){
+        this.edad = edad;
+    }
+
+    public void setHora(byte hora){
+        this.hora = hora;
+    }
+
+    public void setSueldo(double sueldo){
+        this.sueldo = sueldo;
+    }

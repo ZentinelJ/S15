28ed03017b8f47878cb05e3a6ec6d81217467856
villa <@ZentinelJ>
2025-06-05 09:45:17 -0500

    Generamos el constructor de objetos para la clase Persona.

--- src/Persona.java
+++ src/Persona.java
@@ -7,3 +7,13 @@ public class Persona {
     private byte edad;
     private byte hora;
     private double sueldo;
+
+    public Persona(String nombre, String apellido, String genero, String cargo, byte edad, byte hora, double sueldo){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.cargo = cargo;
+        this.edad = edad;
+        this.hora = hora;
+        this.sueldo = sueldo;
+    }
